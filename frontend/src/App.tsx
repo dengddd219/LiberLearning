9a3d9bcba3ae8be27de85cc853b2369ad22ba113
dengddd219 <1984125509@qq.com>
2026-04-14 14:31:54 +0800

    feat: wrap app in TabsProvider, mount global TopBar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,10 @@
+// frontend/src/App.tsx
 import { Routes, Route, Navigate } from 'react-router-dom'
+import { TabsProvider } from './context/TabsContext'
+import TopBar from './components/TopBar'
 import LobbyPage from './pages/LobbyPage'
 import SessionPage from './pages/SessionPage'
+import LiveSessionPage from './pages/LiveSessionPage'
 import UploadPage from './pages/UploadPage'
 import ProcessingPage from './pages/ProcessingPage'
 import NotesPage from './pages/NotesPage'
@@ -8,15 +12,19 @@ import DetailedNotePage from './pages/DetailedNotePage'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LobbyPage />} />
-      <Route path="/session" element={<SessionPage />} />
-      <Route path="/upload" element={<UploadPage />} />
-      <Route path="/processing" element={<ProcessingPage />} />
-      <Route path="/notes/:sessionId" element={<NotesPage />} />
-      <Route path="/notes/detail/:sessionId" element={<DetailedNotePage />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <TabsProvider>
+      <TopBar />
+      <Routes>
+        <Route path="/"                        element={<LobbyPage />} />
+        <Route path="/session"                 element={<SessionPage />} />
+        <Route path="/session/live"            element={<LiveSessionPage />} />
+        <Route path="/upload"                  element={<UploadPage />} />
+        <Route path="/processing"              element={<ProcessingPage />} />
+        <Route path="/notes/:sessionId"        element={<NotesPage />} />
+        <Route path="/notes/detail/:sessionId" element={<DetailedNotePage />} />
+        <Route path="*"                        element={<Navigate to="/" replace />} />
+      </Routes>
+    </TabsProvider>
   )
 }
 
